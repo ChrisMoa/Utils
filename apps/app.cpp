bcd766852d623eff5f964dbbd7511e49039095bb
ChrisMoa <99679805+ChrisMoa@users.noreply.github.com>
2022-11-05 21:08:09 +0100

    Initial commit

---
+++ apps/app.cpp
@@ -0,0 +1,19 @@
+#include <lib.hpp>
+
+#include <fmt/format.h>
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+int main() {
+    std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
+
+    auto [mean, moment] = accumulate_vector(input);
+
+    fmt::print("Mean: {}, Moment: {}\n",  mean, moment);
+    
+    
+    fmt::report_system_error(0,"main()");
+    return 0;
+}
